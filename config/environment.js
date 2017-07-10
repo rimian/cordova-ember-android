87122761079de810d7843e3a27ef033b3b0b3006
Rimian Perkins <hello@rimian.com.au>
2017-07-11 05:36:47 +1000

    You must specify the locationType as hash in your environment.js or rename it to defaultLocationType.

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'cordova-ember-android-d',
     environment: environment,
     rootURL: '/',
-    locationType: 'auto',
+    defaultLocationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
